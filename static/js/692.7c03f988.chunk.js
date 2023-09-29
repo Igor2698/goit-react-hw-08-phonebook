1f6a448332ff249e5281af2c2110b99cb837971c
Igor2698 <128493043+Igor2698@users.noreply.github.com>
2023-09-29 13:07:38 +0000

    Deploying to gh-pages from @ Igor2698/goit-react-hw-08-phonebook@5c2b9a806298d69aea4d0c6010aad1365289ac67 🚀

---
+++ static/js/692.7c03f988.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[692],{7692:function(t,h,a){a.d(h,{NzD:function(){return e}});var c=a(9983);function e(t){return(0,c.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"m13 16 5-4-5-4v3H4v2h9z"}},{tag:"path",attr:{d:"M20 3h-9c-1.103 0-2 .897-2 2v4h2V5h9v14h-9v-4H9v4c0 1.103.897 2 2 2h9c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2z"}}]})(t)}}}]);
+//# sourceMappingURL=692.7c03f988.chunk.js.map
